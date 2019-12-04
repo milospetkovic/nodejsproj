68bc420b447af480c47b43ebdffec659a54a4dc8
milospetkovic <elitasoft.web@gmail.com>
2019-12-04 13:29:29 +0100

    every module is wrapped in so called module wrapper function (example in comments)

--- logger.js
+++ logger.js
@@ -1,11 +1,24 @@
-var url = 'http://elitasoft.com';
+//(function (exports, require, module, __filename, __dirname) {
 
-function log(message) {
-    console.log(message);
-}
+    console.log(__filename);
 
-// export log function
-module.exports.log = log;
+    console.log(__dirname);
 
-// export url variable (endpoint)
-module.exports.endPointUrl = url;
+
+    var url = 'http://elitasoft.com';
+
+    function log(message) {
+        console.log(message);
+    }
+
+    // export log function
+    module.exports.log = log;
+
+    // export url variable (endpoint)
+    module.exports.endPointUrl = url;
+
+//});
+
+
+
+
