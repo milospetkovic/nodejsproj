9ed278fd048ef5d1e49a41c6d29db55763a53d99
milospetkovic <elitasoft.web@gmail.com>
2019-12-04 13:41:55 +0100

    gitignore

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
-var logger = require('./logger');
+// var logger = require('./logger');
+//
+// console.log(logger);
 
-console.log(logger);
+const path = require('path');
+
+var pathObj = path.parse(__filename);
+
+console.log(pathObj);
