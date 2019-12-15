0077bbc9a90d6401b0104b53d965e3a4ea2dae54
milospetkovic <elitasoft.web@gmail.com>
2019-12-15 14:34:12 +0100

    done task - creating callback

--- async-demo/index.js
+++ async-demo/index.js
@@ -1,11 +1,15 @@
 console.log('Before');
 getUser(1, (user) => {
     console.log('USER params: ', user);
+    console.log('now get user\'s github repos');
+    getUserGithubRepos(user.id, function(repos) {
+        console.log('User repositories: ', repos);
+    })
 });
 
 console.log('After');
 
-// 3 Types of asynch calling
+// 3 Types of async calling
 // 1. Callbacks
 // 2. Promises
 // 3. Async/await
@@ -15,5 +19,12 @@ function getUser(id, callback) {
     setTimeout(() => {
         console.log('Reading a user from a database...');
         callback({ id: id, github: 'milos' });
+    }, 2000);    
+}
+
+// get user repositories
+function getUserGithubRepos(id, calback) {
+    setTimeout(() => {
+        calback(['repo1', 'repo2', 'repo3']);
     }, 2000);    
 }
