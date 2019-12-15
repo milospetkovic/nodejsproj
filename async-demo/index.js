c3ba13bd752edebcc57c11d6c6509ccf95d6ba44
milospetkovic <elitasoft.web@gmail.com>
2019-12-15 15:01:33 +0100

    added one more callback function

--- async-demo/index.js
+++ async-demo/index.js
@@ -4,6 +4,15 @@ getUser(1, (user) => {
     console.log('now get user\'s github repos');
     getUserGithubRepos(user.id, function(repos) {
         console.log('User repositories: ', repos);
+        
+        console.log('now read user commits...')
+        getUserRepoCommites(user.name, (commits) => {            
+            if (commits.length > 0) {
+                console.log('User\s commits: ', commits);
+            } else {
+                console.log('User doesn\'t have commits in the repo');
+            }
+        })
     })
 });
 
@@ -18,7 +27,7 @@ console.log('After');
 function getUser(id, callback) {
     setTimeout(() => {
         console.log('Reading a user from a database...');
-        callback({ id: id, github: 'milos' });
+        callback({ id: id, name: 'milos' });
     }, 2000);    
 }
 
@@ -27,4 +36,14 @@ function getUserGithubRepos(id, calback) {
     setTimeout(() => {
         calback(['repo1', 'repo2', 'repo3']);
     }, 2000);    
+}
+
+function getUserRepoCommites(username, commits) {
+    setTimeout((username) => {
+        console.log('Here will be developed connecting to the repo and get commits');
+        //let commits_arr = [];
+        let commits_arr = ['commit1', 'commit2', 'commit3' ];
+        commits(commits_arr);
+    }, 
+    2000);
 }
