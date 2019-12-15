8bb8937985346ec62df085ab15d5efbc295d0961
milospetkovic <elitasoft.web@gmail.com>
2019-12-15 14:24:37 +0100

    simple example of callback example (async calling)

--- async-demo/index.js
+++ async-demo/index.js
@@ -1,6 +1,19 @@
 console.log('Before');
-setTimeout(() => {
-    console.log('time out executing..');
-}, 2000);
+getUser(1, (user) => {
+    console.log('USER params: ', user);
+});
 
 console.log('After');
+
+// 3 Types of asynch calling
+// 1. Callbacks
+// 2. Promises
+// 3. Async/await
+
+// callback
+function getUser(id, callback) {
+    setTimeout(() => {
+        console.log('Reading a user from a database...');
+        callback({ id: id, github: 'milos' });
+    }, 2000);    
+}
