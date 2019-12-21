4532916738b2834807b57fb9418e7f85de69dabd
milospetkovic <elitasoft.web@gmail.com>
2019-12-21 16:23:41 +0100

    read all courses from mongodb

--- mongo-demo/index.js
+++ mongo-demo/index.js
@@ -31,4 +31,16 @@ async function createCourse() {
     }
 }
 
-createCourse();
+async function getCourses() {
+    //const courses = await Course.find();
+    const courses = await Course
+    .find({author: 'milos', isPublished: true})
+    .limit(10)
+    .sort({name: 1})
+    .select({ name: 1, tags: 1})    ;
+    console.log(courses);
+}
+
+//createCourse();
+
+getCourses();
